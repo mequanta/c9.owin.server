891bd00c7e7241f68bd97a2449cf76f3cfb1d704
alex <alex@localhost.localdomain>
2015-03-06 12:50:25 +0800

    test enabled

--- server.js
+++ server.js
@@ -9,8 +9,15 @@ module.exports = main;
 
 function main(options, imports, register) {
     var querystring = require("querystring");
+    var fs = require("fs");
     var api = imports.api;
     var owin = require("connect-owin");
+    if (fs.existsSync(options.testPage)) {
+        api.get("/_signalr/test.html", function(req, res, next) { 
+           var data = fs.readFileSync(options.testPage);
+           res.end(data);
+        });
+    }
 
     api.all("/signalr/*", function(req, res, next) {
         // A hack
